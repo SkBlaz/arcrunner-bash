4b291bd084dcb39f2805fd0422cbb4a327821fcd
skblaz <skrljblaz@gmail.com>
2020-02-20 10:47:36 +0100

    a simple version of arctool

---
+++ arcrunnerbash.sh
@@ -0,0 +1,13 @@
+## Ddistribution
+## inputs: a folder of .xrsl jobs, cluster_id, timeout of refresh
+## outputs a folder_meta with job descriptions, a folder folder_outputs with outputs
+## 1.) first submit
+## 2.) while loop that: a) goes to folder_outputs, arcget -i ../folder_meta/jids.txt
+
+XRSL_FOLDER=$1;CLUSTER=$2;TIMEOUT=$3
+XRSL_META=$XRSL_FOLDER"_META/current.xml";
+XRSL_RESULTS=$XRSL_FOLDER"_RESULTS";
+mkdir XRSL_META XRSL_RESULTS;
+cd XRSL_FOLDER; arcsub -c $CLUSTER -o "../"XRSL_META;
+cd ../XRSL_RESULTS;
+while sleep $TIMEOUT; do arcstat -i "../"XRSL_META & arcget -i "../"XRSL_META; done
