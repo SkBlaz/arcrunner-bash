a8b16738b12640fa83b859f4430644fc4584a6db
skblaz <skrljblaz@gmail.com>
2020-02-20 11:15:53 +0100

    proxy thing

--- arcrunnerbash.sh
+++ arcrunnerbash.sh
@@ -1,10 +1,10 @@
 ## Ddistribution
-## inputs: a folder of .xrsl jobs, cluster_id, timeout of refresh
+## inputs: a folder of .xrsl jobs, cluster_id, timeout of refresh, proxy
 ## outputs a folder_meta with job descriptions, a folder folder_outputs with outputs
 ## 1.) first submit
 ## 2.) while loop that: a) goes to folder_outputs, arcget -i ../folder_meta/jids.txt
 
-XRSL_FOLDER=$1;CLUSTER=$2;TIMEOUT=$3
+XRSL_FOLDER=$1;CLUSTER=$2;TIMEOUT=$3;PROXY=$4;
 mkdir $XRSL_FOLDER"_META";
 XRSL_META=$XRSL_FOLDER"_META/current.xml";
 XRSL_RESULTS=$XRSL_FOLDER"_RESULTS";
@@ -12,4 +12,4 @@ mkdir $XRSL_META $XRSL_RESULTS;
 cd $XRSL_FOLDER;
 arcsub -c $CLUSTER -o "../"$XRSL_META;
 cd ../$XRSL_RESULTS;
-while sleep $TIMEOUT; do [ $(arcstat -i "../"XRSL_META|grep 'no jobs' | wc -l) > 0 ] && break; arcget -i "../"XRSL_META; done
+while sleep $TIMEOUT; do arcproxy -S $PROXY;[ $(arcstat -i "../"XRSL_META|grep 'no jobs' | wc -l) > 0 ] && break; arcget -i "../"XRSL_META; done
