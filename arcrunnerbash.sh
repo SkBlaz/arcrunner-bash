0dfaf57368afc19f594ebfc829c1725a0f7b4df9
skblaz <skrljblaz@gmail.com>
2020-03-10 09:42:55 +0100

    unfold

--- arcrunnerbash.sh
+++ arcrunnerbash.sh
@@ -4,4 +4,21 @@
 ## 1.) first submit
 ## 2.) while loop that: a) goes to folder_outputs, arcget -i ../folder_meta/jids.txt
 
-XRSL_FOLDER=$1;CLUSTER=$2;TIMEOUT=$3;PROXY=$4; mkdir $XRSL_FOLDER"_META"; XRSL_META=$XRSL_FOLDER"_META/current.txt"; touch $XRSL_META; XRSL_RESULTS=$XRSL_FOLDER"_RESULTS"; mkdir $XRSL_RESULTS; cd $XRSL_FOLDER; arcsub -c $CLUSTER -o "../"$XRSL_META *.xrsl; cd ../$XRSL_RESULTS; while sleep $TIMEOUT; do arcproxy -S $PROXY; PVAR=$(arcstat -i "../"$XRSL_META|grep 'no jobs' | wc -l); echo "Prepared to finish: $PVAR"; if [ $PVAR -gt 0 ]; then echo "Breaking with $PVAR" && break; fi; arcget -i "../"$XRSL_META; done
+XRSL_FOLDER=$1;
+CLUSTER=$2;
+TIMEOUT=$3;
+PROXY=$4;
+mkdir $XRSL_FOLDER"_META";
+XRSL_META=$XRSL_FOLDER"_META/current.txt";
+touch $XRSL_META; XRSL_RESULTS=$XRSL_FOLDER"_RESULTS";
+mkdir $XRSL_RESULTS; cd $XRSL_FOLDER; arcsub -c $CLUSTER -o "../"$XRSL_META *.xrsl;
+cd ../$XRSL_RESULTS;
+while sleep $TIMEOUT; do
+    arcproxy -S $PROXY;
+    PVAR=$(arcstat -i "../"$XRSL_META|grep 'no jobs' | wc -l);
+    echo "Prepared to finish: $PVAR";
+    if [ $PVAR -gt 0 ]; then
+	echo "Breaking with $PVAR" && break;
+    fi;
+    arcget -i "../"$XRSL_META;
+done
